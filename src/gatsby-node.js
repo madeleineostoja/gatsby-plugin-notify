164d29906b4022a402f3ce8177cb7e18bed6b62d
Sean King <sean@seanking.org>
2019-09-03 12:56:09 +1200

    Include assets for notification icon

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -2,6 +2,7 @@ const WebpackNotifierPlugin = require('webpack-notifier');
 const path = require('path');
 
 exports.onCreateWebpackConfig = ({ actions }) => {
+  console.log(path.join(__dirname, 'assets/gatsby-icon.png'));
   actions.setWebpackConfig({
     plugins: [
       new WebpackNotifierPlugin({
